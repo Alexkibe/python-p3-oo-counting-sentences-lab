99dad34837a6a1c33dac6198eec401dadb43cc35
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-05-24 05:59:03 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    
+    def __init__(self, value=""):
+        assert isinstance(value, str), "Value must be a string."
+        self.value = value
+
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        return self.value.count('.') + self.value.count('!') + self.value.count('?')
+
+  
+  
+
